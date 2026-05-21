bc4eca023c5dfab95edad389897a1bd489b0b417
Sofiane NOUNOU <sofiane.nounou@atos.net>
2026-05-21 10:43:40 +0100

    add point de vente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,12 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
-import { CreancierListComponent } from './features/creancier/creancier-list.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CreancierListComponent],
+  imports: [RouterOutlet, RouterLink, NzButtonModule, NzLayoutModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,38 @@ import { CreancierListComponent } from './features/creancier/creancier-list.comp
 
 export const routes: Routes = [
   {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'creanciers',
+  },
+  {
     path: 'creanciers',
     component: CreancierListComponent,
     pathMatch: 'full',
+  },
+  {
+    path: 'points-de-vente',
+    loadComponent: () =>
+      import('./features/points-de-vente-list/points-de-vente-list.component').then(
+        (module) => module.PointsDeVenteListComponent
+      ),
+  },
+  {
+    path: 'points-de-vente/new',
+    loadComponent: () =>
+      import('./features/point-de-vente-form/point-de-vente-form.component').then(
+        (module) => module.PointDeVenteFormComponent
+      ),
+  },
+  {
+    path: 'points-de-vente/:id/edit',
+    loadComponent: () =>
+      import('./features/point-de-vente-form/point-de-vente-form.component').then(
+        (module) => module.PointDeVenteFormComponent
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: 'creanciers',
   },
 ];
